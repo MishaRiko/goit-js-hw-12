a9979d567cae078833166de5f9144384201fdb0c
MishaRiko <bmishariko@gmail.com>
2025-08-02 20:14:21 +0300

    Edit   form name

--- src/main.js
+++ src/main.js
@@ -15,7 +15,7 @@ import iziToast from 'izitoast';
 import 'izitoast/dist/css/iziToast.min.css';
 
 // Ініціалізація
-const form = document.querySelector('.search-form');
+const form = document.querySelector('.form');
 const loadMoreBtn = document.querySelector('.load-more');
 let currentPage = 1;
 let currentQuery = '';
